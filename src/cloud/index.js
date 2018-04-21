16f8308f2e417e78c58b42b5170ee9b2335f3915
Leonardo Vieira <leovieira20@gmail.com>
2018-04-21 15:05:06 +0100

    feat: profile get by id

--- src/cloud/index.js
+++ src/cloud/index.js
@@ -1,6 +1,7 @@
-import {get as ProfileGet} from './profile/profile-functions';
+import {get as ProfileGet, getById as ProfileGetById} from './profile/profile-functions';
 import {get as ProjectGet} from './project/project-functions';
 
 Parse.Cloud.define('Profile.get', ProfileGet);
-Parse.Cloud.define('Project.get', ProjectGet);
+Parse.Cloud.define('Profile.getById', ProfileGetById);
 
+Parse.Cloud.define('Project.get', ProjectGet);
--- src/cloud/profile/profile-functions.js
+++ src/cloud/profile/profile-functions.js
@@ -10,3 +10,14 @@ export const get = (req, res) => {
         .then(profiles => profiles.map(p => Profile.mapFromParse(p)))
         .then(profiles => res.success(profiles));
 };
+
+export const getById = (req, res) => {
+    const profileId = req.params.profileId;
+
+    const User = Parse.Object.extend('User');
+    const query = new Parse.Query(User);
+
+    query.get(profileId)
+        .then(p => Profile.mapFromParse(p))
+        .then(profile => res.success(profile));
+};

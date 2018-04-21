75502b45c50450d56fba9cb6012fb43f2b0594e2
Leonardo Vieira <leovieira20@gmail.com>
2018-04-21 16:56:18 +0100

    feat: returning more data from profile

--- src/cloud/domain/model/Profile.js
+++ src/cloud/domain/model/Profile.js
@@ -3,17 +3,31 @@ export default class Profile {
         this.id = args.id;
         this.firstName = args.firstName;
         this.lastName = args.lastName;
+        this.profilePictureUrl = args.profilePictureUrl;
+        this.about = args.about;
+        this.skills = args.skills;
+        this.location = {
+            country: args.country,
+            city: args.city,
+            location: args.location
+        }
     }
 
     static mapFromParse(x) {
-        const id = x.id;
-        const firstName = x.get('firstName');
-        const lastName = x.get('lastName');
-
         return new Profile({
-            id,
-            firstName,
-            lastName
+            id: x.id,
+            firstName: x.get('firstName'),
+            lastName: x.get('lastName'),
+            profilePictureUrl: this.getProfilePictureUrl(x.get('facebookId')),
+            about: x.get('about'),
+            country: x.get('country'),
+            city: x.get('city'),
+            skills: x.get('skills'),
+            location: x.get('location')
         });
+    }
+
+    static getProfilePictureUrl(facebookId) {
+        return `http://graph.facebook.com/${facebookId}/picture?type=large`;
     }
 }

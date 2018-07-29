9675c000a1bc627552da8b27235d823d639ed9a5
Leonardo Vieira <leovieira20@gmail.com>
2018-07-29 15:17:07 +0100

    refactor: return object from profile mapping instead of class

--- src/cloud/domain/model/Profile.js
+++ src/cloud/domain/model/Profile.js
@@ -1,30 +1,18 @@
 module.exports = class Profile {
-    constructor(args = {}) {
-        this.id = args.id;
-        this.firstName = args.firstName;
-        this.lastName = args.lastName;
-        this.about = args.about;
-        this.skills = args.skills;
-        this.profilePictureUrl = args.profilePictureUrl;
-        this.location = {};
-
-        this.location.country = args.country || undefined;
-        this.location.city = args.city || undefined;
-        this.location.location = args.location;
-    }
-
     static mapFromParse(x) {
-        return new Profile({
+        return {
             id: x.id,
             firstName: x.get('firstName'),
             lastName: x.get('lastName'),
             about: x.get('about'),
-            country: x.get('country'),
-            city: x.get('city'),
             skills: x.get('skills'),
             profilePictureUrl: this.getProfilePictureUrl(x),
-            location: x.get('location'),
-        });
+            location: {
+                location: x.get('location'),
+                country: x.get('country') || undefined,
+                city: x.get('city') || undefined,
+            }
+        };
     }
 
     static getProfilePictureUrl(parseProfile) {

df1410b77a5270500055410f3f79f408c2165e34
Leonardo Vieira <leovieira20@gmail.com>
2019-01-07 21:35:37 +0000

    fix: layer functions

--- src/cloud/functions/layer-functions.js
+++ src/cloud/functions/layer-functions.js
@@ -1,10 +1,10 @@
-const root = __dirname;
 const fs = require('fs');
-const layer = require('./layer-parse-module/layer-module');
+const layer = require('../layer-parse-module/layer-module');
+const path = require('path');
 
 const layerProviderID = 'layer:///providers/2fdb6962-5286-11e5-b8db-919013011873'; // Should have the format of layer:///providers/<GUID>
 const layerKeyID = 'layer:///keys/cb92e4ca-617c-11e5-852a-72c700000104'; // Should have the format of layer:///keys/<GUID>
-const privateKey = fs.readFileSync(root + '/layer-parse-module/keys/layer-key.js').toString();
+const privateKey = fs.readFileSync(path.resolve(__dirname, '../layer-parse-module/keys/layer-key')).toString();
 
 layer.initialize(layerProviderID, layerKeyID, privateKey);
 
@@ -16,8 +16,8 @@ const generateToken = (request, response) => {
     if (!currentUser) throw new Error('You need to be logged in!');
 
     response.success(layer.layerIdentityToken(currentUser, nonce));
-});
+};
 
 module.exports = {
     generateToken
-}
+};

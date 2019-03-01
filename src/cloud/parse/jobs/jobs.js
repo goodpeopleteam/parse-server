08690d3cd6989ab49da295e90ec78744ac289a23
Leonardo Vieira <leovieira20@gmail.com>
2019-03-01 16:21:20 +0000

    chore: split profile and project fix jobs

--- src/cloud/parse/jobs/jobs.js
+++ src/cloud/parse/jobs/jobs.js
@@ -8,6 +8,24 @@ Parse.Cloud.job('mapProfiles', ProfileJobs.MapProfiles);
 Parse.Cloud.job('fixProjectUserReference', ProjectJobs.FixProjectUserReference);
 Parse.Cloud.job('normalizeProjects', ProjectJobs.normalizeProjects);
 
+Parse.Cloud.job('refresh projects', async (req) => {
+    const projectCount = ProjectService.count;
+    const promises = await JobBase(projectCount, "Projects", async p => {
+        return p.save();
+    });
+
+    await Promise.all(promises);
+});
+
+Parse.Cloud.job('refresh profiles', async (req) => {
+    const profileCount = UserService.count;
+    const createTalentsFromProfilePromises = await JobBase(profileCount, "User", async p => {
+        return p.save(null, { useMasterKey: true });
+    });
+
+    await Promise.all(createTalentsFromProfilePromises);
+});
+
 Parse.Cloud.job('refresh user and projects', async (req, status) => {
     const projectCount = ProjectService.count;
     const createTalentsFromProjectPromises = await JobBase(projectCount, "Projects", async p => {

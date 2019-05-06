6bcf79e7ab52efc13e12ea8400e72450be7df9cb
leonardo Vieira <leovieira20@gmail.com>
2019-05-06 14:13:15 +0100

    fix(get-project-uc): add v1 fallback to get user's ID

--- src/cloud/domain/usecases/project/get-project-uc.js
+++ src/cloud/domain/usecases/project/get-project-uc.js
@@ -1,10 +1,18 @@
 const ProjectService = require("../../service/ProjectService");
 const Project = require("../../model/Project");
 
+function getOwnerId(project) {
+    const owner = project.get('user');
+    if(owner)
+        return owner.id;
+
+    return project.get('userID');
+}
+
 module.exports.execute = async (user, id) => {
     const project = await ProjectService.getById(id);
 
-    if (project.get('user').id !== user.id) {
+    if (getOwnerId(project) !== user.id) {
         project.increment('views');
         project.save();
     }

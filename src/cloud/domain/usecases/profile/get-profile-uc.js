cbda686fc5bd85daaefb73155c83b3e811d0058a
Leonardo Vieira <leovieira20@gmail.com>
2019-11-05 19:19:36 +0000

    fix(get-profile): removing incremental profile views
    - it's a separate call from the client

--- src/cloud/domain/usecases/profile/get-profile-uc.js
+++ src/cloud/domain/usecases/profile/get-profile-uc.js
@@ -1,19 +1,8 @@
 const UserService = require("../../service/UserService");
 const User = require("../../model/User");
 
-function processProfile(loggedUser, profile) {
-    // profile.isOwnProfile = profile.id === loggedUser.id;
-
-    if (!profile.isOwnProfile) {
-        profile.increment('views');
-        profile.save(null, { useMasterKey: true });
-        // profile.isFavorite = _.find(loggedUser.get('favorites'), f => f.id === profile.id) !== undefined;
-    }
-}
-
 const getById = async (loggedUser, profileId) => {
     const profile = await UserService.getById(profileId);
-    processProfile(loggedUser, profile);
 
     return User.mapFromParse(loggedUser, profile);
 };
@@ -24,7 +13,6 @@ const getByEmail = async (loggedUser, profileEmail) => {
         return null;
     }
 
-    processProfile(loggedUser, profile);
     return User.mapFromParse(loggedUser, profile);
 };
 

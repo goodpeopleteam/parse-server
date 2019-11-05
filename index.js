500311b8094c338192919a7aa9427d4c826aa971
Leonardo Vieira <leovieira20@gmail.com>
2019-11-05 20:31:24 +0000

    feat(settings): setup email provider

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ const api = Parse.ParseServer({
     cloud: './src/cloud/index.js',
     appId: process.env.APP_ID,
     masterKey: process.env.MASTER_KEY,
-    serverURL: `${process.env.SERVER_URL}/parse`
+    serverURL: `${process.env.SERVER_URL}/parse`,
+    publicServerURL: `${process.env.SERVER_URL}/parse`,
+    appName: 'GoodPeople',
+    emailAdapter: {
+        module: '@parse/simple-mailgun-adapter',
+        options: {
+            fromAddress: process.env.FROM_ADDRESS || 'heythere@goodpeopleapp.com.br',
+            domain: 'goodpeopleapp.com.br',
+            apiKey: 'key-37ae3fac3da8d1849ce8f140ca2a16a0'
+        }
+    }
 });
 
 app.use('/parse', api);

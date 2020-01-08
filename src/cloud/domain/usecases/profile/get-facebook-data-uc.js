1886df08a652b0b9bb1a5187dca2483a42e600f9
Leonardo Vieira <leovieira20@gmail.com>
2020-01-08 19:18:43 +0000

    fix: including facebookId for backward compatibility with older versions

--- src/cloud/domain/usecases/profile/get-facebook-data-uc.js
+++ src/cloud/domain/usecases/profile/get-facebook-data-uc.js
@@ -11,6 +11,7 @@ module.exports.execute = async user => {
     const url = `https://graph.facebook.com/v3.2/${fbData.id}?fields=email%2Cfirst_name%2Clast_name%2Clocation&access_token=${fbData.access_token}`;
     const response = await (await fetch(url)).json();
 
+    user.set('facebookId', fbData.id);
     user.set('email', response.email || user.get('email'));
     user.set('firstName', response.first_name || user.get('firstName'));
     user.set('lastName', response.last_name || user.get('lastName'));
